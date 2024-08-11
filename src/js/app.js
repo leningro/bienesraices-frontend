377d972616d9df9c264d1471382fcf23ce86971f
leningro <gro.gtz.lenin@hotmail.com>
2024-08-11 13:46:46 -0600

    Añadiendo DarkMode

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     eventListeners();
+    darkMode();
 })
+
+function darkMode() {
+    const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+    prefiereDarkMode.addEventListener("change", function() {
+        //Lo metimos aquí para hacerlo "reactivo"
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    })
+
+    const botonDarkMode = document.querySelector(".dark-mode-boton");
+    
+    botonDarkMode.addEventListener("click", function(){
+        document.body.classList.toggle("dark-mode");
+    })
+}
 
 function eventListeners() {
     const mobileMenu = document.querySelector(".mobile-menu");
