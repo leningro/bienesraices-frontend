2fa27bf4547a314b5244cfefe0b6409d8271e560
leningro <gro.gtz.lenin@hotmail.com>
2024-08-19 20:06:23 -0600

    php codigo espaguetti - Update del CRUD

--- admin/index.php
+++ admin/index.php
@@ -20,8 +20,10 @@
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raíces</h1>
 
-        <?php if( intval( $resultado ) === 1): ?>
+        <?php if( intval( $resultado ) === 1 ): ?>
             <p class="alerta exito">Anuncio Creado Correctamente</p>
+        <?php elseif( intval( $resultado ) === 2 ): ?>
+            <p class="alerta exito">Anuncio Actualizado Correctamente</p>
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton-verde">Nueva Propiedad</a>
@@ -45,8 +47,8 @@
                     <th> <img src="../imagenes/<?php echo $propiedad['imagen']; ?>" alt="imagen propiedad" class="imagen-tabla"> </th>
                     <th>$<?php echo $propiedad['precio']; ?></th>
                     <th>
-                        <a href="#" class="boton-rojo-block">Actualizar</a>
-                        <a href="#" class="boton-amarillo-block">Eliminar</a>
+                        <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
                     </th>
                 </tr>
                 <?php endwhile; ?>
